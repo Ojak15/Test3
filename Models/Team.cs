0f1ceec55fb002bf3512d7175fba2d2531dd306d
Ojak15 <77587376+Ojak15@users.noreply.github.com>
2022-12-07 18:24:55 +0000

    Add files via upload

---
+++ Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+
+namespace Test2.Models
+{
+    public class Team
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public string? LeagueName { get; set; }
+
+        public string? BasketballNiId { get; set; }
+
+
+    }
+}
